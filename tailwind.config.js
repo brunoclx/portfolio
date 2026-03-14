b147b87e026e2257d3fde4ee2fa20d7d90da3057
brunoclx <brunocalix@gmail.com>
2026-03-14 10:45:16 -0300

    Adicionando a font Outfit como padrão do projeto

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,11 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./*.html"],
+  content: ["./*.html", "./src/**/*.html", "./assets/**/*.html"],
   theme: {
+    fontFamily: {
+      sans: ["Outfit", "sans-serif"], // Outfit substitui a fonte padrão
+    },
     extend: {},
   },
   plugins: [],
 }
-
